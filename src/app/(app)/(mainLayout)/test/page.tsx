1a24fd35233c3f05c34a67d3a7b9991cc50640ab
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2024-04-22 22:59:48 +0200

    update

--- src/app/(app)/(mainLayout)/test/page.tsx
+++ src/app/(app)/(mainLayout)/test/page.tsx
@@ -2,22 +2,19 @@
 import React from 'react'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
+import parse from 'html-react-parser'
 import './style.css'
 type Props = {}
 
 const page = async (props: Props) => {
-  const parse = require('html-react-parser').default
   const payload = await getPayload({ config: configPromise })
-
   const data = await payload.find({
     collection: 'pages',
   })
+  const parseDat = await parse(data.docs[0].nameOfYourRichTextField_html)
   return (
     <div>
-      <div>
-        {parse(typeof data.docs[0].nameOfYourRichTextField_html === 'undefined' && '') ===
-          'undefined' && ''}
-      </div>
+      <div>{parseDat}</div>
     </div>
   )
 }
