015c5ba845b7d55c485eb4373ffec22137fa249c
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2024-04-23 10:10:15 +0200

    update

--- src/app/(app)/(mainLayout)/test/page.tsx
+++ src/app/(app)/(mainLayout)/test/page.tsx
@@ -4,14 +4,18 @@ import { getPayloadHMR } from '@payloadcms/next/utilities'
 import configPromise from '@payload-config'
 import parse, { domToReact } from 'html-react-parser'
 import Image from 'next/image'
+import { unstable_cache } from 'next/cache'
 import './style.css'
 type Props = {}
-
-const page = async (props: Props) => {
+const getPages = unstable_cache(async () => {
   const payload = await getPayloadHMR({ config: configPromise })
   const data = await payload.find({
     collection: 'pages',
   })
+  return data
+}, 'pages')
+const page = async (props: Props) => {
+  const data = await getPages()
   const parseDat = parse(data?.docs[0]?.nameOfYourRichTextField_html, {
     replace({ name, children, attribs }) {
       if (name === 'img') {
