4290a7b33acb30920322e71c6087c7a20f4f9b2f
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2024-04-27 11:58:21 +0200

    update

--- src/app/components/mainLayout/FetchNavContent.tsx
+++ src/app/components/mainLayout/FetchNavContent.tsx
@@ -14,13 +14,13 @@ import configPromise from '@payload-config'
 const FetchNavContent = async () => {
   noStore()
   const payload = await getPayloadHMR({ config: configPromise })
-  const data = await payload.findGlobal({
+  const data: any = await payload.findGlobal({
     slug: 'nav',
     depth: 4,
   })
   return (
     <div className="py-5">
-      {data.nav.map((item) => {
+      {data.nav.map((item: any) => {
         return (
           <Link href={'/' + item.id} key={item.id}>
             <div key={item.id} className="w-full h-20">
