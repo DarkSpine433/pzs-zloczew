c26a3afd95e1975eac5543dcccc6257ffe76ecca
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2024-04-27 15:19:47 +0200

    update

--- src/app/(app)/(mainLayout)/[...slug]/page.tsx
+++ src/app/(app)/(mainLayout)/[...slug]/page.tsx
@@ -1,9 +1,32 @@
+//@ts-nocheck
 import React from 'react'
-
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import configPromise from '@payload-config'
+import BlockParser from '@/app/components/BlockParser'
+import './style.css'
 type Props = {}
 
-const page = (props: Props) => {
-  return <div>page</div>
+const page = async () => {
+  const payload = await getPayloadHMR({ config: configPromise })
+  const data = await payload.find({
+    collection: 'pages',
+  })
+
+  const dataDisplay = data.docs.map((doc) =>
+    doc?.Content.map((block) => {
+      return (
+        <>
+          <BlockParser block={block} />
+        </>
+      )
+    }),
+  )
+
+  return (
+    <div>
+      <div>{dataDisplay}</div>
+    </div>
+  )
 }
 
 export default page
