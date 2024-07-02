378eb36b1b26768d756b2651b9266ff1862f7519
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2024-07-02 11:11:06 +0200

    UPDATE:   svg

--- src/app/components/news/FavouriteButtonClient.tsx
+++ src/app/components/news/FavouriteButtonClient.tsx
@@ -145,14 +145,14 @@ const FavouriteButtonClient = ({ id, isBlock, className }: Props) => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  strokeWidth="1.5"
+                  strokeWidth={1.5}
                   stroke="currentColor"
-                  className={`size-6 ${arrayOfFavouriteItems !== undefined && arrayOfFavouriteItems.includes(id) ? "fill-primary" : ""}`}
+                  className="size-6"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                    d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5"
                   />
                 </svg>
               </Button>
