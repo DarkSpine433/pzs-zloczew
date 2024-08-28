dc911423491db136ae4618e8d684d3260c9139e8
Dawid Konopiaty <dawidkonopiaty@gmail.com>
2024-08-28 20:48:57 +0200

    contact layout update

--- src/app/(app)/(mainLayout)/(dynamicRoutes)/contact/page.tsx
+++ src/app/(app)/(mainLayout)/(dynamicRoutes)/contact/page.tsx
@@ -10,19 +10,23 @@ const page = (props: Props) => {
   return (
     <>
       {" "}
-      <div className="after:to-gray-black/30 relative z-10 block space-y-5 overflow-hidden bg-cover bg-center bg-no-repeat py-5 pb-14 after:absolute after:inset-0 after:left-0 after:top-0 after:z-[-5] after:block after:h-full after:w-full after:bg-background after:bg-gradient-to-tr after:from-foreground after:from-40% after:to-foreground/40 after:content-[''] sm:space-y-10 sm:py-20">
-        <h1 className="bg-[url('/img/news/grain.jpg')] bg-clip-text text-center text-[4rem] font-extrabold tracking-widest text-background/40 sm:text-7xl md:text-8xl lg:text-9xl">
+      <div className="relative z-10 block space-y-5 overflow-hidden border-b-4 border-primary bg-foreground px-10 py-5 pb-14 sm:space-y-10 sm:py-12">
+        <h1 className="lg:text-9xl= bg-[url('/img/news/grain.jpg')] bg-clip-text text-center text-[4rem] font-extrabold tracking-widest text-background/40 sm:text-7xl md:text-8xl">
           Kontakt
         </h1>
-        <h3 className="mx-auto max-w-4xl px-6 text-left text-sm tracking-widest text-primary [text-shadow:0_0_5px_blue] md:text-base">
-          Oto nasze dane kontaktowe, abyś mógł się z nami skontaktować.
+        <h3 className="mx-auto max-w-4xl border-l border-gray-400 px-4 py-1 text-left text-sm tracking-widest text-gray-500 first-letter:uppercase md:text-base [&>*]:first-letter:font-extrabold [&>*]:first-letter:uppercase [&>*]:first-letter:text-primary">
+          <div>
+            Oto nasze dane kontaktowe, abyś mógł się z nami skontaktować.
+          </div>
           <br />
-          <br />
-          Skontaktuj się z nami! Czekamy na Twój telefon lub e-mail, abyśmy
-          mogli Ci pomóc.
+
+          <div>
+            Skontaktuj się z nami! Czekamy na Twój telefon lub e-mail, abyśmy
+            mogli Ci pomóc.
+          </div>
         </h3>
       </div>
-      <div className="mx-auto min-h-[600px] max-w-6xl px-3 pt-10">
+      <div className="mx-auto min-h-[600px] max-w-6xl border-x px-3 pt-10">
         <FetchContent />
       </div>
     </>
